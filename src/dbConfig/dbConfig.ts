2063cc2f52685e2dbf71d86e5f45199e61ab56d8
Mamitiana <mamitiana@fulldigits.com>
2023-08-18 06:26:58 +0300

    44:33 to tell TS that process.env.MONGO_URI will be always there

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI);
+        mongoose.connect(process.env.MONGO_URI!);
     }catch(error) {
         console.log('Something goes wrong!, Connection to DB');
         console.log(error);
