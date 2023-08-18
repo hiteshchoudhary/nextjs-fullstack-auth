cd470e0cf6fefa24c5438a9dfd700d4c1de1ed46
Mamitiana <mamitiana@fulldigits.com>
2023-08-18 06:29:55 +0300

    46:4 Code of Connection to the MongoDB

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,6 +2,16 @@ import mongoose from 'mongoose';
 export async function connect() {
     try {
         mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('MongoDB Connected successfully');
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
     }catch(error) {
         console.log('Something goes wrong!, Connection to DB');
         console.log(error);
