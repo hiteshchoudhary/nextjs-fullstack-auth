0502bb11697609a64b826e0a07c2084423f8c4a6
Mamitiana <mamitiana@fulldigits.com>
2023-08-19 19:00:16 +0300

    51:45 Importing everthing that is needed by src/app/signup/page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,10 @@
-export default function LoginPage() {
+"use client";
+import Link from "next/Link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import {axios} from "axios";
+
+export default function SignupPage() {
     return(
         <div className="flex">
             <h1 className="text-center text-white text-2xl">Signup</h1>
