dfa431532021c00ff9457d02db9dd9596bd78f80
Mamitiana <mamitiana@fulldigits.com>
2023-08-19 19:04:43 +0300

    52:38 Here we do the fn that is going the signup, it is done after we accumulated all the data that are needed for signup, - We need that async bcz we are going to grab data from DB

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,7 +9,10 @@ export default function SignupPage() {
         email: "",
         password: "",
         username: ""
-    });
+    })
+    const onSignup = async() => {
+
+    }
     return(
         <div className="flex">
             <h1 className="text-center text-white text-2xl">Signup</h1>
