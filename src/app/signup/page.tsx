06694bdf43800ddf00d41a6c2f036e24682126bc
Mamitiana <mamitiana@fulldigits.com>
2023-08-19 20:04:49 +0300

    56:42 password should be XX

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -37,7 +37,7 @@ export default function SignupPage() {
 
             <label htmlFor="password">password</label>
             <input className="p-2 border border-grary-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" 
-                type="text" 
+                type="password" 
                 id="password"
                 value={user.password}
                 onChange={(e) => setUser({...user, password: e.target.value})}
