625e4fd3d42ce50ac8db1b1dc4e92af145b19b91
Mamitiana <mamitiana@fulldigits.com>
2023-08-19 20:22:10 +0300

    57:30 Creating button signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -43,6 +43,11 @@ export default function SignupPage() {
                 onChange={(e) => setUser({...user, password: e.target.value})}
                 placeholder="password"
             />
+            <button
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            >
+                Signup here
+            </button>
         </div>
     )
 }
