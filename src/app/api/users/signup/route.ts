d08217e64b41ac89c21976e4fb4aff1e0f22af17
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 22:47:45 +0300

    1:30:35 we transform the request of POST into json, and we save it into a var

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -8,7 +8,7 @@ connect()
 
 export async function POST(request: NextRequest){
     try {
-        
+        const reqBody = await request.json()
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
 
