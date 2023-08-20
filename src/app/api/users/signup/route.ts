e5ceb1ff89257f174e6170d8cffb33f8f8f52561
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 22:31:02 +0300

    1:28:56 connect() is from dbConfig.ts, once again, we are going to make a request to db which is on other continent, then we use "async"

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,3 +2,8 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+
+connect()
+
+
+export async 
