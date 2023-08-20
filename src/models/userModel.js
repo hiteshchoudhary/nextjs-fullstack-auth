6fd4a2ef8f963ade85628c443aa1fe41cc4e0c1f
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 21:38:49 +0300

    1:15:48 Field to check is that user an admin ?

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 })
 
 const User = mongoose.models.users || mongoose.model("users", userSchema);
