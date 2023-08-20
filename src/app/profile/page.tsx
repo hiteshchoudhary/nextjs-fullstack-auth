7e0856267cd6e27795b6dc31ab03fda0a93d4792
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 12:36:22 +0300

    1:2:38 Start writing the app/profile/page.tsx - Centering the basic text

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile page</p>
+        </div>
+    )
+}
