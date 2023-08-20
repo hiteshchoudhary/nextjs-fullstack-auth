93ee0d88556cf4ff941043ec4028748ee8e8e856
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 21:35:14 +0300

    1:14:00 Long explanation about the model of mongo const userSchema = new mongoose.Schema({     username: {         type: String,         required: [true, "Please provide a username"],         unique: true,     },   ... })

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,20 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
 })
 
 const User = mongoose.models.users || mongoose.model("users", userSchema);
