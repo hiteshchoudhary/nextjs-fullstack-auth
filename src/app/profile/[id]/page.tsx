1dee3a30a2edcd1e8f23249cd0860fb2a4c0fbfa
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 12:47:35 +0300

    1:5:50 This is the page in localhost:3000/profile/1... But not yet printing the 1 in /profile/1

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default function UserProfile() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p
+                className="text-4xl"
+            >
+                Profile page
+            </p>
+        </div>
+    )
+}
