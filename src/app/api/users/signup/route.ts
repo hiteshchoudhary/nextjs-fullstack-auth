bd2019cfc2618b9a15acda6f801934042f6cbd6c
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 22:49:21 +0300

    1:31:2 Here we extract the (username, mail, password) from the PostRequestBody

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -9,6 +9,7 @@ connect()
 export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json()
+        const {username, email, password} = reqBody
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
 
