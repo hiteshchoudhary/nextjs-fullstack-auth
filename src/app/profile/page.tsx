9a3a95edfd0377977666b3d4311534c3e6978db5
Mamitiana <mamitiana@fulldigits.com>
2023-08-20 12:49:08 +0300

    The displayed page that is shown in /profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,7 @@ export default function ProfilePage() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <p>Profile page</p>
+            <p>Main Profile page</p>
         </div>
     )
 }
