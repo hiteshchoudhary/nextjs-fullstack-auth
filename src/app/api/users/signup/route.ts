fb443810318b4ac36ec43d0bf69f49218661469b
Mamitiana <mamitiana@fulldigits.com>
2023-08-21 07:11:17 +0300

    1:32:35, if the user exists already in /signup, then return nextResponse_json_error

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -15,6 +15,10 @@ export async function POST(request: NextRequest){
 
         //check if user already exists
         const user = await User.findOne({email})
+        
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
 
