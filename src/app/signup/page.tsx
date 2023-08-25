9650b885227a49084e5f719e5c73391083d8dc85
Mamitiana <mamitiana@fulldigits.com>
2023-08-25 04:24:29 +0300

    1:44:40 Showing message when there is error during signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/Link";
 import React, { useEffect }  from "react";
 import { useRouter } from "next/navigation";
 import {axios} from "axios";
+import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
     const router = useRouter();
@@ -17,7 +18,9 @@ export default function SignupPage() {
         try {
             
         } catch (error) {
+            console.log("Signup failed", error.message);
             
+            toast.error(error.message);
         }finally {
             setLoading(false);
         }
