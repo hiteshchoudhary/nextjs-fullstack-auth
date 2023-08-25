18933105cf3893d97072be2cae83c847deaf96fd
Mamitiana <mamitiana@fulldigits.com>
2023-08-25 04:38:22 +0300

    Printing the MONGO_URI when connecting to the DB

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 export async function connect() {
     try {
+        console.log("process.env.MONGO_URI: " + process.env.MONGO_URI)
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
 
