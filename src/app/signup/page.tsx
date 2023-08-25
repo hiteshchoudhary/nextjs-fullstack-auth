1f146e871f498fe92088249184de4a8099928b09
Mamitiana <mamitiana@fulldigits.com>
2023-08-25 04:27:10 +0300

    1:46:24 When signingUp, setLoading=true, send request to /api/users/signup, route to /login

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/Link";
 import React, { useEffect }  from "react";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
+import axios from "axios";
 import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
@@ -16,7 +16,10 @@ export default function SignupPage() {
     const [loading, setLoading] = React.useState(false);
     const onSignup = async() => {
         try {
-            
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
         } catch (error) {
             console.log("Signup failed", error.message);
             
