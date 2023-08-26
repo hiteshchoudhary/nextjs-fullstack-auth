a2ecedd792d68d6a8eab1df8c40f8bc7ff37ffc1
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 06:49:32 +0300

    1:51:18 Remember, all of your /Src/api/XXX/route.ts needs to connect to the MongoDB

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,3 +2,5 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+
+connect()
