52ba7555f4891c484723879dca6b85979ce18b22
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 10:45:47 +0300

    2:0:21 To have almost the same code of /src/app/login/page.tsx in the video

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,6 @@ import { toast } from "react-hot-toast";
 
 
 export default function LoginPage() {
-    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
@@ -21,27 +20,8 @@ export default function LoginPage() {
 
 
     const onLogin = async () => {
-        try {
-            setLoading(true);
-            const response = await axios.post("/api/users/login", user);
-            console.log("Login success", response.data);
-            toast.success("Login success");
-            router.push("/profile");
-        } catch (error:any) {
-            console.log("Login failed", error.message);
-            toast.error(error.message);
-        } finally{
-        setLoading(false);
-        }
     }
 
-    useEffect(() => {
-        if(user.email.length > 0 && user.password.length > 0) {
-            setButtonDisabled(false);
-        } else{
-            setButtonDisabled(true);
-        }
-    }, [user]);
 
     return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
