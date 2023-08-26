c6bd2e4b9954432c870eda6911d5332e16e58519
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 16:04:31 +0300

    2:30:0 SLD=Somehow Long Description: if netuser1 is having cookie_token and is accessing the publicUrl, then he can access that publicUrl

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,10 @@ export function middleware(request:  NextRequest) {
 
     const token = request.cookies.get('token')?.value || '' 
 
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
 }
  
 // See "Matching Paths" below to learn more
