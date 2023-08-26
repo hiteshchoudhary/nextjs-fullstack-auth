6d282354dc5c4cfd6e4a289d95f266d9812e2a19
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 15:29:47 +0300

    2:21:10 After we clicked button_logout, then we are going to be redirected to 3000/login

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,11 +2,15 @@
 import axios from "axios";
 import Link from "next/link";
 import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
 
 export default function ProfilePage() {
+    const router = useRouter()
     const logout = async  () => {
         try {
             await axios.get('/api/users/logout')
+            toast.success('Logout successful')
+            router.push('/login')
         } catch (error:any) {
             console.log(error.message);
             toast.error(error.message)
