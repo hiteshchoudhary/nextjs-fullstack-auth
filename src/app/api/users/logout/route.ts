af849d7ef5e4dbbf9d13b43fd7c77c082516a1fd
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 12:31:57 +0300

    2:15:10 Here to manage if THERE is error while entering in 3000/api/users/logout

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,3 +1,9 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {}
+export async function GET() {
+    try {
+        
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
