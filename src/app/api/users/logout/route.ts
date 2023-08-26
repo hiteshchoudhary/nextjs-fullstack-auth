55e9b0064e825bfd981dfc4f9b20fff0a4f001af
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 12:45:25 +0300

    2:15:51 Creating the response while loggingOut... And going to craft that response

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,7 +2,12 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
     } catch (error:any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
