93a63c322ad54ca0cb0b71cb351774424a576309
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 18:44:16 +0300

    2:40:48 Grabbing data from DB where _id(atDb)=idFromCookieToken

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,7 +9,7 @@ connect();
 export async function GET( request:NextRequest){
     try {
         const userId = await getDataFromToken(request);
-        
+        const user = await User.findOne({_id: userId});
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 400});
     }
