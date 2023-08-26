6f6b6ac789b389af7f38c610e3f2ce45cefd8ffc
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 06:54:39 +0300

    1:52:15 Grabbing the data that comes to 3000/api/users/login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -7,7 +7,10 @@ connect()
 
 export async function POST(request: NextRequest){
     try {
-        
+        const reqBody = await request.json()
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
     }
