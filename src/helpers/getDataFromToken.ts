99d7f16af4adcf32876ffc90ac75df1ba4ec2caa
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 17:39:10 +0300

    2:36:37 Decode the token

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || '';
+        const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!); 
     } catch (error: any) {
         throw new Error(error.message);
     }
