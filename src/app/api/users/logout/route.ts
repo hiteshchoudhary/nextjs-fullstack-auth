b054819a3cfbcbea098ac1349f1c86d3968b6f1b
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 14:41:58 +0300

    2:16:15 Deleting the cookie_token in the netuser, you have to do it inside the response that we prepared before... Do not forget to return the response

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -8,6 +8,10 @@ export async function GET() {
                 success: true, 
             } 
         ) 
+        response.cookies.set("token", "", { 
+            httpOnly: true, expires: new Date(0) 
+        });
+        return response;
     } catch (error:any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
