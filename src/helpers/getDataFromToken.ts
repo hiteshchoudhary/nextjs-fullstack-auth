9c3e9b222d66f6f2524167e444658f30332b86af
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 17:40:51 +0300

    2:37:18 Grabbing the decodedToken.id that comes from src/api/users/login/route.ts > tokenData.id

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -5,6 +5,7 @@ export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || '';
         const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!); 
+        return decodedToken.id;
     } catch (error: any) {
         throw new Error(error.message);
     }
