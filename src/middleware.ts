4ddd28ebbaa0235bbc4d2538de4d1bcdb35b4d30
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 15:57:37 +0300

    2:28:7 Defining the public_uri which we will use in our middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request:  NextRequest) {
     const path = request.nextUrl.pathname
+    const isPublicPath = path === '/login' || path === '/signup'
 }
  
 // See "Matching Paths" below to learn more
