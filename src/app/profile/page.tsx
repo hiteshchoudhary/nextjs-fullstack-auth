c0917d2ddb49202b8888d2981d70b5d09539fcb0
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 22:20:55 +0300

    Adding button in 3000/profile that will show all info of the logged_in user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -27,6 +27,12 @@ export default function ProfilePage() {
                 onClick={logout}
                 className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >Logout</button>
+
+
+            <button
+                className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >GetUser Details
+            </button>
         </div>
     )
 }
