579950da44f92e0669dbd5121e05e152f37f7174
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 15:52:01 +0300

    2:26:55 This part of src/middleware.ts is the logic of our middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // See "Matching Paths" below to learn more
