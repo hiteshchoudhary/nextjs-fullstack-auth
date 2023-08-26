54078190c06cd94185d581ad108ebc89ce5405c9
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 10:47:37 +0300

    2:0:24 we use useRouter() bcz we are going to push into another different page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,6 +10,7 @@ import { toast } from "react-hot-toast";
 
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
