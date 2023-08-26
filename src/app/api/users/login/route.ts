7af81bc20244ba107ca9c506f685ad5b4376af2a
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 07:09:41 +0300

    1:54:17 This is goign to grab all data from DB that is related to reqBody.email reqBody is from 3000/users/login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -12,7 +12,7 @@ export  async function POST(request: NextRequest){
         console.log(reqBody);
 
         //check if user exists
-        const user = await User.findOne({email})
+        const user = await User.findOne({email}) 
         if(!user){
             return NextResponse.json({error: "User does not exist"}, {status: 400})
         }
