6d632def2ebb4ac05158583cbcaec13a0f11b03e
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 15:25:56 +0300

    2:20:13 you clicked button_logout and here we manage if there is error in that click, that might be net_error_connection or something else

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import {toast} from "react-hot-toast";
+
 export default function ProfilePage() {
-    const logout = async () => {}
+    const logout = async () => {
+        try {
+        } catch (error:any) {
+            console.log(error.message);
+            toast.error(error.message)
+        } 
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
