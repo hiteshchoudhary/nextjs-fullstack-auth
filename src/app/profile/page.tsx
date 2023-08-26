e4b6aaeaa6a5c3ca43e3c3501845eeca6b678300
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 14:49:02 +0300

    Creating the fn logout that will be run when you clicked button_logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 export default function ProfilePage() {
+    const logout = async () => {}
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
@@ -7,6 +8,7 @@ export default function ProfilePage() {
             <hr />
 
             <button
+                onClick={logout}
                 className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >Logout</button>
         </div>
