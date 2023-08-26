2cf98ef27f220592a540e096ce87f6c4421af1fa
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 07:02:11 +0300

    Do not forget, if you are grabbing data from DB, then do not forget the "async & await"

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -5,12 +5,12 @@ import bcryptjs from "bcryptjs";
 
 connect()
 
-export async function POST(request: NextRequest){
+export  async function POST(request: NextRequest){
     try {
-        const reqBody = await request.json()
+        const reqBody =  await request.json()
         const {email, password} = reqBody;
         console.log(reqBody);
-        
+
         //check if user exists
         const user = await User.findOne({email})
         if(!user){
