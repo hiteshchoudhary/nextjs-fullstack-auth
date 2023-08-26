de318ead3e2ef13b2d405b742fc0a89add28d057
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 06:59:36 +0300

    1:53:9 LT=LongText: Remember, 3000/api/users/login got reqBody from 3000/users/login 3000/api/users/login is going to treat the data of login - This code is going to check if the reqBody.email exists - if the req.email=user does not exist then going to return error400

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -10,7 +10,13 @@ export async function POST(request: NextRequest){
         const reqBody = await request.json()
         const {email, password} = reqBody;
         console.log(reqBody);
-
+        
+        //check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+        console.log("user exists");
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
     }
