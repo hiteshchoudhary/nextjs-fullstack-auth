3dd7fffb4186cfb35cac813ca33f8ba7a882a139
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 17:35:56 +0300

    2:35:55 If there is error in getDataFromToken() then throw error

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,4 +1,10 @@
 import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-export const getDataFromToken = (request: NextRequest) => {}
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
