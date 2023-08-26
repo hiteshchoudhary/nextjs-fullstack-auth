69cda41a074e3458a66f369b228013682636b726
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 06:51:00 +0300

    1:51:44 "export async function POST(request: NextRequest){" is very used in /src/app/api/XXX/route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -4,3 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 
 connect()
+
+export async function POST(request: NextRequest){
+    try {
+        
+    } catch (error: any) {
+        
+    }
+}
