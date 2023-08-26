ef026b44e8dc44734b1fb9c838b105ff421f2878
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 14:47:46 +0300

    2:18:46 Creating a button for logout... Shown in the 3000/profile, 3000/profile is shown where you are loggedIn

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,11 @@ export default function ProfilePage() {
             <h1>Profile</h1>
             <hr />
             <p>Main Profile page</p>
+            <hr />
+
+            <button
+                className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >Logout</button>
         </div>
     )
 }
