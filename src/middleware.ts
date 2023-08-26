47e4529d3c0f9849adca8eb4fdf32cec71d20384
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 16:00:48 +0300

    Explanation of the "?"... If the cookie_token is present then var_token=cookie_token, if cookie_token is missing then var_token=""

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request:  NextRequest) {
     const path = request.nextUrl.pathname
     const isPublicPath = path === '/login' || path === '/signup'
 
-    const token = request.cookies.get('token')?.value || ''
+    const token = request.cookies.get('token')?.value || '' 
 
 }
  
