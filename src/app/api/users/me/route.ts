e3da5ecaef1f6c00595bd6ad53b500ac0619a96f
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 18:45:47 +0300

    2:41:24 SLD=Somehow Long Description: prev code is going to give everything from user1AtDb, but we need everything EXCLUDING the password... it possible to do ("-password -email") Note that we filter from the field from /src/app/signup/page.tsx

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,7 +9,7 @@ connect();
 export async function GET( request:NextRequest){
     try {
         const userId = await getDataFromToken(request);
-        const user = await User.findOne({_id: userId});
+        const user = await User.findOne({_id: userId}).select("-password");
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 400});
     }
