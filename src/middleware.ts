45a0850f5031b5a69641cd433d89d20dbb41130c
Mamitiana <mamitiana@fulldigits.com>
2023-08-26 15:56:43 +0300

    2:27:43 This NextRequest1 is allowing to check in which URL are you !

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export function middleware(request:  NextRequest) {
+    const path = request.nextUrl.pathname
 }
  
 // See "Matching Paths" below to learn more
