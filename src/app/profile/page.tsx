b25a120500bba1cb1a016c624575e4fa1a4294c9
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 07:05:43 +0300

    2:45:20 LD: As summary 3000/api/users/me returns 1 row=user1 from DB, going to do state_data=user1._id but that user1 from 3000/api/users/me is {mesaaage:"User found", data={email: "", username: "", ...}} 3000/api/users/me ret {mesaaage="", data=RowFromwDB={_id:"", username="", ...}=checkMongoDBRow=check/src/models/userModel.js+_idField}

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import React, {useState} from "react";
 import {toast} from "react-hot-toast";
 import {useRouter} from "next/navigation";
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("nothing")
     const logout = async  () => {
         try {
             await axios.get('/api/users/logout')
@@ -18,6 +20,8 @@ export default function ProfilePage() {
     }
     const getUserDetails = async () => {
         const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id) 
     }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
