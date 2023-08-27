0628ba427ab19a0ec322280ed45b66f570e46691
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 14:05:50 +0300

    3:17:8 Finding in the DB where field_verifyToken=cookie_token and field_verifyTokenExpiry is NOT yet expired

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -10,6 +10,9 @@ export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json()
         const {token} = reqBody
+        console.log("token @app/api/users/verifyemail/route.ts: "+token)
+
+        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
         
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 500})
