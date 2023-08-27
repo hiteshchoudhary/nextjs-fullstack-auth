a21cbdbc3355138a7786149b19e0bdce9d8dc617
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 12:19:22 +0300

    3:2:28 Managing if there is error in sendMailer

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -3,5 +3,9 @@ import User from "@/models/userModel";
 import bcryptjs from 'bcryptjs';
 
 export const sendEmail = async({email, emailType, userId}:any) => {
-    
+    try {
+        
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
 }
