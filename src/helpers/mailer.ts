edbe58a104b1851a77d76804caa001ac803060e8
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 13:37:25 +0300

    3:11:56 Code to send the mail and returning the status of that sending mail when calling the file src/helpers/mailer.ts

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -34,13 +34,16 @@ export const sendEmail = async({email, emailType, userId}:any) => {
         });
 
         const mailOptions = {
-            from: 'hitesh@gmail.com',
+            from: 'mamitianasolofo@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
             or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
             </p>`
         }
+
+        const mailresponse = await transport.sendMail(mailOptions);
+        return mailresponse;
         
     } catch (error: any) {
         throw new Error(error.message);
