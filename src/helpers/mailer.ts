f38a031bfe470ba9dde41f014e6549b5c5e11c15
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 12:56:24 +0300

    3:5:50 Find a row in db based on _id and update the field_verifyToken, field_verifyTokenExpiry in the db, this is really tied to /src/models/userModel.js

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -6,6 +6,13 @@ export const sendEmail = async({email, emailType, userId}:any) => {
     try {
         // create a hased token
         const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+        await User.findByIdAndUpdate(
+            userId, 
+            {
+                verifyToken: hashedToken, 
+                verifyTokenExpiry: Date.now() + 3600000
+            }
+        )
         
     } catch (error: any) {
         throw new Error(error.message);
