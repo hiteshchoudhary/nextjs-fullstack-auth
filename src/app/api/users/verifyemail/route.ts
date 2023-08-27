abc4fa054c7991cf4ed546ec6d1373cea7677566
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 13:46:48 +0300

    3:15:22 3000/api/users/verifyemail is going to be called by axios in post_way... + if calling 3000/api/users/verifyemail generates error then going to display error500

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -4,3 +4,13 @@ import User from "@/models/userModel";
 
 
 connect()
+
+
+export async function POST(request: NextRequest){
+    try {
+        
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    
+    }
+}
