7df9d61fb9afc0f54eb70dd372fbc817ddbef7b9
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 12:07:15 +0300

    Minor fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,7 +20,7 @@ export default function SignupPage() {
             const response = await axios.post("/api/users/signup", user); 
             console.log("Signup success", response.data);
             router.push("/login");
-        } catch (error) {
+        } catch (error: any) {
             console.log("Signup failed", error.message);
             
             toast.error(error.message);
