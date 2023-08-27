5e2707f86eb3b3b6c7616cd6a12844dc3101bb5a
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 14:02:24 +0300

    3:16:10 3000/api/users/verifyemail/route.ts is going to be called like this: axios.post("/api/users/verifyemail", {token})

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -8,6 +8,8 @@ connect()
 
 export async function POST(request: NextRequest){
     try {
+        const reqBody = await request.json()
+        const {token} = reqBody
         
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 500})
