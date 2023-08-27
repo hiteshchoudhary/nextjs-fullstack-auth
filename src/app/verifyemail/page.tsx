249df8535d7ab24c71d04732eba1aef3245bda17
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 16:30:41 +0300

    3:35:35 If state_token is present then print the token, otherwise print no token

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -35,7 +35,8 @@ export default function VerifyEmailPage() {
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl">Verify Email</h1>
-            <h2 className="p-2 bg-orange-500 text-black">{token ? "thereTokenDo1" : "no token do2"}</h2>
+            <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "no token"}</h2>
+
 
         </div>
     )
