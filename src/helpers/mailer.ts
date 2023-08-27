c7bf5f62cfca3725ac13f44db2522adc6d24062a
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 13:07:57 +0300

    3:9:8 Grab the code to create a mailTransport(not sending the mail yet ! ) from mailtrap.io > Email Testing > Inboxes > Integration=NodeMailer > Click the Copy

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -23,6 +23,15 @@ export const sendEmail = async({email, emailType, userId}:any) => {
                 }
             )
         }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "dec2cb5f266394",
+              pass: "124851b2db71c9"
+            }
+          });
         
     } catch (error: any) {
         throw new Error(error.message);
