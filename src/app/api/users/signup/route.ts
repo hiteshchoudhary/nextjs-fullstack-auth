45398f1def2d701a48fda34ba772aa755c2a406c
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 14:23:13 +0300

    3:20:48 The mail verification should be sent while you are signing up

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect()
 
@@ -32,6 +33,8 @@ export async function POST(request: NextRequest){
         })
         const savedUser = await newUser.save()
         console.log("savedUser: " + savedUser);
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully",
