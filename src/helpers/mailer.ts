043e771bc47b2d022152b6020b06bf1f043119e1
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 12:16:53 +0300

    3:1:29 LD: Info we need when we want to send mail... Explaining export const sendEmail = async({email, emailType, userId}:any) => { - email is the recipient that is going to receive the mail - emailType: This is going to be enum(verifyMail, forgotPassword, ...) - userId: Grabbing some info about the receiver - :any so we do not really care about the type of the vars in the param

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,3 +1,7 @@
 import nodemailer from 'nodemailer';
 import User from "@/models/userModel";
 import bcryptjs from 'bcryptjs';
+
+export const sendEmail = async({email, emailType, userId}:any) => {
+    
+}
