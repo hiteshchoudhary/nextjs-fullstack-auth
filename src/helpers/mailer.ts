807426fa5f331e35558ddec5757937dc5dfc1531
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 12:55:03 +0300

    3:3:8 Creating a token that will be used for verify_user and reset_password

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -4,6 +4,8 @@ import bcryptjs from 'bcryptjs';
 
 export const sendEmail = async({email, emailType, userId}:any) => {
     try {
+        // create a hased token
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
         
     } catch (error: any) {
         throw new Error(error.message);
