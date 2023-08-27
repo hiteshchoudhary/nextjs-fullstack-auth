4cb0cecca8d504a4594319fe47b0378118b749c1
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 13:17:27 +0300

    3:10:20 Defining the content of the mail that we are going to send - if emayType = VERIFY then mailSubject="Verify your email" else mailSubject="Reset your password" - if emailType = VERIFY then mailBody contains "verify your mail" else it contains "reset your password"

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -31,7 +31,16 @@ export const sendEmail = async({email, emailType, userId}:any) => {
               user: "dec2cb5f266394",
               pass: "124851b2db71c9"
             }
-          });
+        });
+
+        const mailOptions = {
+            from: 'hitesh@gmail.com',
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`
+        }
         
     } catch (error: any) {
         throw new Error(error.message);
