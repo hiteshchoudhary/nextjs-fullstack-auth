710270cd8054bfeea20c53f9ade4e0772d03f0f4
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 16:10:11 +0300

    3:29:30 state_verified, state_error for the app/verifyemail/page.tsx

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -7,4 +7,7 @@ import React, { useEffect, useState } from "react";
 
 export default function VerifyEmailPage() {
     const [token, setToken] = useState("");
+    const [verified, setVerified] = useState(false);
+    const [error, setError] = useState(false);
+
 }
