f8a0c97e9cf50de78fda329fdede453522f2d189
Mamitiana <mamitiana@fulldigits.com>
2023-08-27 16:23:43 +0300

    3:34:10 LD: 3000/verifyemail?token=XXX is coming into /src/app/verifyemail/page.tsx - > useEffect(()=>{}, []) will catch it, and it will build the state_token - > when state_token got value then going to run useEffect(()=>{}, [token])

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -10,4 +10,25 @@ export default function VerifyEmailPage() {
     const [verified, setVerified] = useState(false);
     const [error, setError] = useState(false);
 
+    const verifyUserEmail = async () => {
+        try {
+            await axios.post('/api/users/verifyemail', {token})
+            setVerified(true);
+        } catch (error:any) {
+            setError(true);
+            console.log(error.reponse.data);
+        }
+
+    }
+
+    useEffect(() => {
+        const urlToken = window.location.search.split("=")[1];
+        setToken(urlToken || "");
+    }, []);
+
+    useEffect(() => {
+        if(token.length > 0) {
+            verifyUserEmail();
+        }
+    }, [token]);
 }
