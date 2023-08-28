1e904c5f43a3e6dbeab60ca610b8733801d34a09
Mamitiana <mamitiana@fulldigits.com>
2023-08-28 05:49:14 +0300

    Doing some print at src/app/api/users/forgotpassword/route.ts

--- src/app/api/users/forgotpassword/route.ts
+++ src/app/api/users/forgotpassword/route.ts
@@ -10,19 +10,25 @@ export async function POST(request: NextRequest){
         const reqBody = await request.json() 
         const {email} = reqBody
         const user = await User.findOne({email})
-        if(user){
-            return NextResponse.json({error: "User already exists"}, {status: 400})
-        }
-
+        
         const random = (Math.random() + 1).toString(36).substring(7);
         console.log("Random: " + random)
+        console.log("user._id: " + user._id)
+        console.log("user.email: " + user.email)
+
         const tokenData = {
             random: random,
             email: user.email
         }
         const token =  await jwt.sign(tokenData,  process.env.TOKEN_SECRET!, {expiresIn: "1d"})
-
+        console.log("token: " + token)
+        return NextResponse.json({
+            message: "token of forgotten password created successfully",
+            success: true,
+            token
+        })
     } catch (error: any) {
+        console.log("Error at src/app/api/users/forgotpassword/route.ts")
         return NextResponse.json({error: error.message}, {status: 500})
     }
 }
