235a802e014f5a74b82fca8f8ace46e801841cc9
Mamitiana <mamitiana@fulldigits.com>
2023-08-28 04:58:30 +0300

    if user_mail from 3000/forgotpassword does Not exist then return error400

--- src/app/api/users/forgotpassword/route.ts
+++ src/app/api/users/forgotpassword/route.ts
@@ -1,11 +1,17 @@
 import {connect} from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
 
 connect()
 
 export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json() 
+        const {email} = reqBody
+        const user = await User.findOne({email})
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
     }
