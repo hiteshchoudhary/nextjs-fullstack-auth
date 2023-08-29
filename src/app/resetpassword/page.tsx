657d94ce4ea399b32ca5dc8fb9e11af97b9f064f
Mamitiana <mamitiana@fulldigits.com>
2023-08-29 15:08:08 +0300

    3000/resetpassword?token=XXX is printing the token in the console of the browser + 3000/resetpassword?token=XXX > /api/users/resetpassword

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -6,13 +6,28 @@ import React, { useEffect, useState } from "react";
 
 export default function ResetPasswordPage() {
     const [token, setToken] = useState("");
+    const [reset, setReset] = useState(false);
+    const [error, setError] = useState(false);
     
     const resetPassword = async () => {
-
+        try {
+            await axios.post('/api/users/resetpassword', {token}) 
+            setReset(true); 
+        } catch (error:any) {
+            setError(true);
+            console.log(error.response.data);
+        }
     }
 
     useEffect(() => {
         const urlToken = window.location.search.split("=")[1];
+        console.log("urlToken: " + urlToken)
         setToken(urlToken || ""); 
     }, []);
+
+    useEffect(() => {
+        if(token.length > 0) {
+            resetPassword();
+        }
+    }, [token]);
 }
