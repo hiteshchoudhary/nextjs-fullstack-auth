62d7999f5a74e0e894da340b6f1762509e3d95de
Mamitiana <mamitiana@fulldigits.com>
2023-08-30 15:58:48 +0300

    Basic code of 3000/createnewpass

---
+++ src/app/createnewpass/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import React, {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+export default function CreateNewPagePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>This is createNewPass</h1>
+        </div>
+    )}
