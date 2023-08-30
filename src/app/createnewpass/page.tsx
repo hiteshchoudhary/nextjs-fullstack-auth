676fd63152bcee77fe1f0ef971e14a0f2f1e48ee
Mamitiana <mamitiana@fulldigits.com>
2023-08-30 19:29:02 +0300

    3000/createnewpass is passing the input_password to createnewpass/route.ts

--- src/app/createnewpass/page.tsx
+++ src/app/createnewpass/page.tsx
@@ -6,24 +6,34 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 export default function CreateNewPagePage() {
-    const [password, setPassword] = useState("")
-    const onReset = async () => {
-        alert("password: " + password)
-        // const response = await axios.post("/api/users/createnewpass", password); 
+    const [password, setPassword] = React.useState({
+        password: "",
+    })
+    const router = useRouter();
+    const onLogin = async () => {
+        try {
+            const response = await axios.post("/api/users/createnewpass", password); 
+            toast.success("Create new pass");
+            router.push("/login");
+        } catch (error: any) {
+            console.log("Create new pass failed", error.message);
+            toast.error(error.message);
+        } 
     }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <label htmlFor="password">New password: </label>
+            <label htmlFor="password">password</label>
             <input 
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="password"
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                value={password.password}
+                onChange={(e) => setPassword({...password, password: e.target.value})}
                 placeholder="password"
             />
             <button
-                onClick={onReset}
+                onClick={onLogin}
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
                     Reset
             </button>
