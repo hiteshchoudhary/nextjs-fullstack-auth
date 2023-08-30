6f43c95e97ebaf3355da720f9cb18325a9b7c376
Mamitiana <mamitiana@fulldigits.com>
2023-08-30 15:54:31 +0300

    Return the user that we just found and having the token1 that we sent in mail

--- src/app/api/users/resetpassword/route.ts
+++ src/app/api/users/resetpassword/route.ts
@@ -6,11 +6,12 @@ import User from "@/models/userModel";
 connect()
 
 export async function POST(request: NextRequest){
+    let user;
     try {
         const reqBody = await request.json()
         const {token} = reqBody
         console.log("token @app/api/users/resetpassword/route.ts: "+token)
-        const user = await User.findOne({forgotPasswordToken: token, forgotPasswordTokenExpiry: {$gt: Date.now()}});
+        user = await User.findOne({forgotPasswordToken: token, forgotPasswordTokenExpiry: {$gt: Date.now()}});
         console.log("user @app/api/users/resetpassword/route.ts: " + user)
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
@@ -19,6 +20,7 @@ export async function POST(request: NextRequest){
     console.log("Here is the end of POST @app/api/users/resetpassword/route.ts")
     return NextResponse.json({
         message: "Password resetted",
-        success: true
+        success: true,
+        user: user
     })
 }
